a97b3f04e3726247c1426dac0c9bffa3d6527a25
Felipe <felipe.pessoa@xpi.com.br>
2020-08-13 20:04:39 -0300

    added try catch

--- app/src/main/java/com/pessoadev/pokedexproject/list/presentation/ListViewModel.kt
+++ app/src/main/java/com/pessoadev/pokedexproject/list/presentation/ListViewModel.kt
@@ -5,17 +5,22 @@ import com.pessoadev.pokedexproject.list.domain.ListRepository
 import com.pessoadev.pokedexproject.list.model.PokeListResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
-class ListViewModel: ViewModel() {
+class ListViewModel : ViewModel() {
 
     //todo - precisar ir para o DI
     private val listRepository = ListRepository()
 
-    var pokeList =  MutableLiveData<PokeListResponse>().apply { mutableListOf<PokeListResponse>() }
+    var pokeList = MutableLiveData<PokeListResponse>().apply { mutableListOf<PokeListResponse>() }
 
     fun getPokeList() {
         viewModelScope.launch {
-            pokeList.value = listRepository.getPokeList()
+            try {
+                pokeList.value = listRepository.getPokeList()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
